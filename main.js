775736551e011fb081b179cbd28ef94d6377687f
Nik K <nk@nikk.ca>
2021-04-23 07:29:57 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+export class AnimQueue {
+    _req = -1;
+    _queue = new Set();
+
+    _callback = (t) => {
+        this._req = -1;
+        const q = [...this._queue];
+        this._queue.clear();
+        for (let i=0; i<q.length; i++) q[i](t);
+    }
+
+    add(f) {
+        this._queue.add(f);
+        if (this._req === -1) this._req = window.requestAnimationFrame(this._callback);
+    }
+
+    delete(f) {
+        this._queue.delete(f);
+        if (this._queue.size === 0) {
+            window.cancelAnimationFrame(this._callback);
+            this._req = -1;
+        }
+    }
+}
