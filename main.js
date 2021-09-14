7217050ba10a93b8c6a57c0da4609bcbc1ecb3b2
Nik K <nk@nikk.ca>
2021-09-13 21:20:04 -0400

    Use private members

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 export class AnimQueue {
-    _req = -1;
-    _queue = new Set();
+    #req = -1;
+    #queue = new Set();
 
-    _callback = (t) => {
-        this._req = -1;
-        const q = [...this._queue];
-        this._queue.clear();
+    #callback = (t) => {
+        this.#req = -1;
+        const q = [...this.#queue];
+        this.#queue.clear();
         for (let i=0; i<q.length; i++) q[i](t);
     }
 
     add(f) {
-        this._queue.add(f);
-        if (this._req === -1) this._req = window.requestAnimationFrame(this._callback);
+        this.#queue.add(f);
+        if (this.#req === -1) this.#req = window.requestAnimationFrame(this.#callback);
     }
 
     delete(f) {
-        if (this._queue.delete(f) && this._queue.size === 0) {
-            window.cancelAnimationFrame(this._req);
-            this._req = -1;
+        if (this.#queue.delete(f) && this.#queue.size === 0) {
+            window.cancelAnimationFrame(this.#req);
+            this.#req = -1;
         }
     }
 }
